906cc80dff4a932d843177d55a771064ffcb80c0
AryoP <aryopatel@gmail.com>
2020-09-18 23:22:07 -0500

    progress

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('welcome.html')
+
+@app.route('/stocks')
+def stocks():
+    return "Stocks page"
+
+@app.route('/learn')
+def learn():
+    return "Learn page"
+
+@app.route('/trends')
+def trends():
+    return "Trends"
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/javascript/animations.js
@@ -0,0 +1,7 @@
+//animation for the welcome message
+anime({
+    targets: '#welcome-main, #welcome-secondary',
+    opacity: [0,1],
+    duration: 1000,
+    easing: 'easeInOutQuad'
+})
